d3450355167c615da2bb96d860d7a0728d80538e
bitanya-gezahegn <99648999+bitanya-gezahegn@users.noreply.github.com>
2024-08-16 02:16:38 -0700

    Create scrape_tiktok_data.py

---
+++ scrape_tiktok_data.py
@@ -0,0 +1,28 @@
+from TikTokApi import TikTokApi
+import requests
+
+def scrape_tiktok_data(request):
+    api = TikTokApi()
+    username = "yenehealth" 
+    user_videos = api.user(username=username).videos(count=5)
+
+    tiktok_data = []
+
+    for video in user_videos:
+        video_url = video.video_url
+        caption = video.caption
+        hashtags = ', '.join([tag.name for tag in video.hashtags])
+        upload_date = video.create_time
+
+        tiktok_data.append({
+            'video_url': video_url,
+            'caption': caption,
+            'hashtags': hashtags,
+            'upload_date': upload_date,
+        })
+
+   
+    google_apps_script_url = 'https://script.google.com/macros/s/AKfycbz9v9KVXYXI_oiJ_WnEt9H21tJxJQUECniVvRAdt86ftyrkdQx_0PPLHEAMVOx0pr45vw/exec'
+    response = requests.post(google_apps_script_url, json=tiktok_data)
+
+    return f"Data sent with status: {response.status_code}"
